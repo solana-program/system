cb9a8c794f7d15d4b31705062148445608d71367
Loris Leiva <loris.leiva@gmail.com>
2024-03-08 14:43:10 +0000

    First commit

---
+++ scripts/generate-clients.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env zx
+import "zx/globals";
+import * as k from "@metaplex-foundation/kinobi";
+import { workingDirectory } from "./utils.mjs";
+
+// Instanciate Kinobi.
+const kinobi = k.createFromIdls([
+  path.join(workingDirectory, "program", "idl.json"),
+]);
+
+// Update instructions.
+kinobi.update(
+  k.updateInstructionsVisitor({
+    createAccount: {
+      byteDeltas: [k.instructionByteDeltaNode(k.argumentValueNode("space"))],
+    },
+  })
+);
+
+// Render JavaScript.
+const jsClient = path.join(__dirname, "..", "clients", "js");
+kinobi.accept(
+  k.renderJavaScriptExperimentalVisitor(
+    path.join(jsClient, "src", "generated"),
+    { prettier: require(path.join(jsClient, ".prettierrc.json")) }
+  )
+);
+
+// Render Rust.
+const rustClient = path.join(__dirname, "..", "clients", "rust");
+kinobi.accept(
+  k.renderRustVisitor(path.join(rustClient, "src", "generated"), {
+    formatCode: true,
+    crateFolder: rustClient,
+  })
+);
