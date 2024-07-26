530355d61a74cea70e53f6f602a6316c4e0f57b6
Loris Leiva <loris.leiva@gmail.com>
2024-07-26 10:23:59 +0100

    wip

--- scripts/upgrade-template.mjs
+++ scripts/upgrade-template.mjs
@@ -4,5 +4,4 @@ import "zx/globals";
 cd("..");
 await $`pnpm create solana-program system --address 11111111111111111111111111111111 --default --force`;
 cd("system");
-await $`git add --all`;
-await $`git restore --staged program/**/* clients/**/generated/**/*`;
+await $`git add --all && git restore --worktree --staged program/**/* clients/**/generated/**/*`;
