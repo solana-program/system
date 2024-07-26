52091248a8732491e14f91ec9b551f6697b38bdc
Loris Leiva <loris.leiva@gmail.com>
2024-07-26 11:23:33 +0100

    wip

--- scripts/upgrade-template.mjs
+++ scripts/upgrade-template.mjs
@@ -3,19 +3,23 @@ import "zx/globals";
 
 $.quote = (command) => command;
 const unchangedGlobs = [
-  "clients/**/src/**/*",
-  "clients/js/test/**/*",
-  "clients/rust/tests/**/*",
+  "clients/**/src/**",
+  "clients/**/src/*",
+  "clients/js/test/*",
+  "clients/rust/tests/*",
   "program/**/*",
+  "program/*",
   "scripts/generate-clients.mjs",
   "scripts/generate-idls.mjs",
   "scripts/upgrade-template.mjs",
-  "scripts/program/**/*",
+  "scripts/program/*",
 ];
 
 cd("..");
 await $`pnpm create solana-program system --address 11111111111111111111111111111111 --default --force`;
 cd("system");
 await $`git add --all`;
-await $`git restore --worktree --staged "${unchangedGlobs}"`;
+for (const glob of unchangedGlobs) {
+  await $`git restore --worktree --staged "${glob}"`;
+}
 await $`pnpm install`;
