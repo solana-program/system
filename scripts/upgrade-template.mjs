657186e5b71935ce8e4c188318987b3363c55d08
Loris Leiva <loris.leiva@gmail.com>
2024-07-26 10:33:09 +0100

    wip

--- scripts/upgrade-template.mjs
+++ scripts/upgrade-template.mjs
@@ -1,21 +1,20 @@
 #!/usr/bin/env zx
 import "zx/globals";
 
+const unchangedPaths = [
+  "clients/**/src/**/*",
+  "clients/**/src/*",
+  "clients/**/test/**/*",
+  "clients/**/test/*",
+  "clients/**/tests/**/*",
+  "clients/**/tests/*",
+  "program/**/*",
+  "program/*",
+  "scripts/program/*",
+  "scripts/upgrade-template.mjs",
+];
+
 cd("..");
 await $`pnpm create solana-program system --address 11111111111111111111111111111111 --default --force`;
 cd("system");
-await $`git add --all`;
-await $`git restore
-  --worktree 
-  --staged 
-  clients/**/src/**/*
-  clients/**/src/*
-  clients/**/test/**/*
-  clients/**/test/*
-  clients/**/tests/**/*
-  clients/**/tests/*
-  program/**/*
-  program/*
-  scripts/program/*
-  scripts/upgrade-template.mjs
-`;
+await $`git add --all && git restore --worktree --staged ${unchangedPaths}`;
