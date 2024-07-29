1902a068ece555567b66c060f1a08efe2306ac92
Loris Leiva <loris.leiva@gmail.com>
2024-07-29 13:31:41 +0100

    Improve upgrade script with lib name overrides (#12)

--- scripts/upgrade-template.mjs
+++ scripts/upgrade-template.mjs
@@ -1,25 +1,58 @@
 #!/usr/bin/env zx
-import "zx/globals";
+import 'zx/globals';
+import { getCargo } from './utils.mjs';
 
-$.quote = (command) => command;
-const unchangedGlobs = [
-  "clients/**/src/**",
-  "clients/**/src/*",
-  "clients/js/test/*",
-  "clients/rust/tests/*",
-  "program/**/*",
-  "program/*",
-  "scripts/generate-clients.mjs",
-  "scripts/generate-idls.mjs",
-  "scripts/upgrade-template.mjs",
-  "scripts/program/*",
+// Arguments to pass to the `create-solana-program` command.
+const rustClientCargo = getCargo(path.join('clients', 'rust'));
+const jsClientPkg = require(
+  path.join(__dirname, '..', 'clients', 'js', 'package.json')
+);
+const templateArgs = [
+  'system',
+  '--address',
+  '11111111111111111111111111111111',
+  '--org',
+  'solana-program',
+  '--rust-client-crate-name',
+  rustClientCargo.package.name,
+  '--js-client-package-name',
+  jsClientPkg.name,
+  '--default',
+  '--force',
 ];
 
-cd("..");
-await $`pnpm create solana-program system --address 11111111111111111111111111111111 --default --force`;
-cd("system");
+// File and folder patterns that should not be overwritten by the template upgrade.
+const unchangedGlobs = [
+  'clients/**/src/**',
+  'clients/**/src/*',
+  'clients/js/test/*',
+  'clients/rust/tests/*',
+  'program/**/*',
+  'program/*',
+  'scripts/generate-clients.mjs',
+  'scripts/generate-idls.mjs',
+  'scripts/upgrade-template.mjs',
+  'scripts/program/*',
+  'Cargo.lock',
+  '**/pnpm-lock.yaml',
+  'pnpm-lock.yaml',
+];
+
+// Prevent CLI arguments from being escaped.
+$.quote = (command) => command;
+
+// Re-generate the repo from the parent directory.
+cd('..');
+await $`pnpm create solana-program@latest ${templateArgs}`;
+
+// Go back inside the updated repo.
+cd('system');
+
+// Restore files and folders that should not be overwritten.
 await $`git add --all`;
 for (const glob of unchangedGlobs) {
   await $`git restore --worktree --staged "${glob}"`;
 }
+
+// Re-install dependencies.
 await $`pnpm install`;
