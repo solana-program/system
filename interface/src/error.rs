05714f4000c1eb212f57705f22a399a387a98ea7
febo <febo@anza.xyz>
2024-11-07 21:16:07 +0000

    Address review comments

--- interface/src/error.rs
+++ interface/src/error.rs
@@ -4,6 +4,7 @@ use solana_decode_error::DecodeError;
 
 // Use strum when testing to ensure our FromPrimitive
 // impl is exhaustive
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(test, derive(strum_macros::FromRepr, strum_macros::EnumIter))]
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
 pub enum SystemError {
