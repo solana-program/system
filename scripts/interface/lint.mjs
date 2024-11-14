4fb5680a164141b0fdc9a054b0a049272c82a0f2
febo <febo@anza.xyz>
2024-11-14 00:05:57 +0000

    Move out format from lint

--- scripts/interface/lint.mjs
+++ scripts/interface/lint.mjs
@@ -1,27 +1,18 @@
 // Script to lint the interface crate.
 //
 // This script runs the following sub-scripts:
-// - lint-format.mjs
 // - lint-clippy.mjs
 // - lint-features.mjs
 // - lint-docs.mjs
 //
-// Command line arguments are not passed to the sub-scripts, apart from `--fix`
-// which is passed to lint-format.mjs and lint-clippy.mjs.
+// Command line arguments are only passed to clippy.
 
 import { cliArguments, popArgument, workingDirectory } from '../utils.mjs';
 
 const scripts = path.join(workingDirectory, 'scripts', 'interface');
 
-// We only need to pass `--fix` to the format and clippy linters. The other
-// arguments are filtered out since tey might not be supported by all sub-scripts.
-const args = cliArguments();
-const fix = popArgument(args, '--fix');
-
-// format
-await $`zx ${path.join(scripts, 'lint-format.mjs')} ${fix ? '--fix' : ''}`;
 // clippy
-await $`zx ${path.join(scripts, 'lint-clippy.mjs')} ${fix ? '--fix' : ''}`;
+await $`zx ${path.join(scripts, 'lint-clippy.mjs')} ${cliArguments()}`;
 // features
 await $`zx ${path.join(scripts, 'lint-features.mjs')}`;
 // rustdoc
