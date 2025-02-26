1e8f40756e3c98ace642112d5acf98f8afb73979
Joe Caulfield <jcaulfield135@gmail.com>
2025-02-26 12:06:57 +0000

    review feedback

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -2,7 +2,6 @@
 
 use {
     solana_account_info::AccountInfo, solana_program_error::ProgramResult, solana_pubkey::Pubkey,
-    solana_system_interface::instruction::SystemInstruction,
 };
 
 pub fn process(_program_id: &Pubkey, _accounts: &[AccountInfo], _input: &[u8]) -> ProgramResult {
