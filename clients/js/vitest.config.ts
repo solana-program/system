f8cee8f111e649544baa74ba541163c8a3b02007
Loris Leiva <loris.leiva@gmail.com>
2026-02-17 17:25:30 +0000

    Bump Vitest test timeout

--- clients/js/vitest.config.ts
+++ clients/js/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'node',
-        testTimeout: 10000,
+        testTimeout: 15000,
     },
 });
